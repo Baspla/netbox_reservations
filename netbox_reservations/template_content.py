d33c2943dcfbbda642f4ca4af64fce71526a5726
Tim Morgner <tim@timmorgner.de>
2023-02-24 14:02:33 +0100

    Simplified Extensions

--- netbox_reservations/template_content.py
+++ netbox_reservations/template_content.py
@@ -12,9 +12,9 @@ class DeviceClaimsExtension(PluginTemplateExtension):
     def x_page(self):
         obj = self.context['object']
         claims = Claim.objects.filter(tag__in=obj.tags.all())
-        claims_table = ClaimTable(claims)
+        claims_table = ExtendedClaimTable(claims)
         return self.render(
-            'netbox_reservations/device_extend.html',
+            'netbox_reservations/extend_claims.html',
             extra_context={
                 'related_claims_table': claims_table
             }
@@ -32,11 +32,12 @@ class TagClaimsExtension(PluginTemplateExtension):
         claims = Claim.objects.filter(tag=obj)
         claims_table = ExtendedClaimTable(claims)
         return self.render(
-            'netbox_reservations/tag_extend_claims.html',
+            'netbox_reservations/extend_claims.html',
             extra_context={
                 'related_claims_table': claims_table
             }
         )
+
 
 class ContactReservationsExtension(PluginTemplateExtension):
     model = 'tenancy.contact'
@@ -49,11 +50,12 @@ class ContactReservationsExtension(PluginTemplateExtension):
         reservations = Reservation.objects.filter(contact=obj)
         reservations_table = ReservationTable(reservations)
         return self.render(
-            'netbox_reservations/contact_extend.html',
+            'netbox_reservations/extend_reservations.html',
             extra_context={
                 'related_reservations_table': reservations_table
             }
         )
+
 
 class TenantReservationsExtension(PluginTemplateExtension):
     model = 'tenancy.tenant'
@@ -66,10 +68,122 @@ class TenantReservationsExtension(PluginTemplateExtension):
         reservations = Reservation.objects.filter(tenant=obj)
         reservations_table = ReservationTable(reservations)
         return self.render(
-            'netbox_reservations/tenant_extend.html',
+            'netbox_reservations/extend_reservations.html',
             extra_context={
                 'related_reservations_table': reservations_table
             }
         )
 
-template_extensions = [DeviceClaimsExtension, TagClaimsExtension, ContactReservationsExtension, TenantReservationsExtension]
+
+class PrefixClaimsExtension(PluginTemplateExtension):
+    model = 'ipam.prefix'
+
+    def right_page(self):
+        return self.x_page()
+
+    def x_page(self):
+        obj = self.context['object']
+        claims = Claim.objects.filter(tag__in=obj.tags.all())
+        claims_table = ExtendedClaimTable(claims)
+        return self.render(
+            'netbox_reservations/extend_claims.html',
+            extra_context={
+                'related_claims_table': claims_table
+            }
+        )
+
+
+class VLANClaimsExtension(PluginTemplateExtension):
+    model = 'ipam.vlan'
+
+    def right_page(self):
+        return self.x_page()
+
+    def x_page(self):
+        obj = self.context['object']
+        claims = Claim.objects.filter(tag__in=obj.tags.all())
+        claims_table = ExtendedClaimTable(claims)
+        return self.render(
+            'netbox_reservations/extend_claims.html',
+            extra_context={
+                'related_claims_table': claims_table
+            }
+        )
+
+
+class InterfaceClaimsExtension(PluginTemplateExtension):
+    model = 'dcim.interface'
+
+    def right_page(self):
+        return self.x_page()
+
+    def x_page(self):
+        obj = self.context['object']
+        claims = Claim.objects.filter(tag__in=obj.tags.all())
+        claims_table = ExtendedClaimTable(claims)
+        return self.render(
+            'netbox_reservations/extend_claims.html',
+            extra_context={
+                'related_claims_table': claims_table
+            }
+        )
+
+
+class VMClaimsExtension(PluginTemplateExtension):
+    model = 'virtualization.virtualmachine'
+
+    def right_page(self):
+        return self.x_page()
+
+    def x_page(self):
+        obj = self.context['object']
+        claims = Claim.objects.filter(tag__in=obj.tags.all())
+        claims_table = ExtendedClaimTable(claims)
+        return self.render(
+            'netbox_reservations/extend_claims.html',
+            extra_context={
+                'related_claims_table': claims_table
+            }
+        )
+
+
+class IPAddressClaimsExtension(PluginTemplateExtension):
+    model = 'ipam.ipaddress'
+
+    def right_page(self):
+        return self.x_page()
+
+    def x_page(self):
+        obj = self.context['object']
+        claims = Claim.objects.filter(tag__in=obj.tags.all())
+        claims_table = ExtendedClaimTable(claims)
+        return self.render(
+            'netbox_reservations/extend_claims.html',
+            extra_context={
+                'related_claims_table': claims_table
+            }
+        )
+
+
+class SiteGroupsClaimsExtension(PluginTemplateExtension):
+    model = 'dcim.sitegroup'
+
+    def right_page(self):
+        return self.x_page()
+
+    def x_page(self):
+        obj = self.context['object']
+        claims = Claim.objects.filter(tag__in=obj.tags.all())
+        claims_table = ExtendedClaimTable(claims)
+        return self.render(
+            'netbox_reservations/extend_claims.html',
+            extra_context={
+                'related_claims_table': claims_table
+            }
+        )
+
+
+template_extensions = [DeviceClaimsExtension, TagClaimsExtension, ContactReservationsExtension,
+                       TenantReservationsExtension, PrefixClaimsExtension, VLANClaimsExtension,
+                       InterfaceClaimsExtension,
+                       VMClaimsExtension, IPAddressClaimsExtension, SiteGroupsClaimsExtension]

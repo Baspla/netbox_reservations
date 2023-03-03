2775d0bea68d1ad532d5552e9ed33319bc09ba9d
Tim Morgner <tim@timmorgner.de>
2023-03-03 11:21:15 +0100

    Fixed collison feature

--- netbox_reservations/util/queries.py
+++ netbox_reservations/util/queries.py
@@ -1,9 +1,22 @@
 from django.db.models import Q
 
+import dcim.models
 from netbox_reservations.models import Reservation
 
 
+# Überschneidungen
+# AStart vor BStart und AEnde nach BStart
+# AStart vor BEnde und AEnde nach BEnde
+# AStart nach BStart und AStart vor BEnde
+
 def getConflictingReservations(instance):
-    return Reservation.objects.filter(
-        Q(end_date__lte=instance.end_date) & Q(end_date__gte=instance.end_date) & ~Q(id=instance.id)
-    ).distinct()
+    return Reservation.objects.filter(~Q(id=instance.id) &
+                                      (
+                                          (Q(start_date__lte=instance.start_date) & Q(
+                                              end_date__gte=instance.start_date)) |
+                                          (Q(start_date__lte=instance.end_date) & Q(end_date__gte=instance.end_date)) |
+                                          (Q(start_date__gte=instance.start_date) & Q(
+                                              start_date__lte=instance.end_date))
+                                      )
+                                      ).distinct()
+
